5248f89ff1e8929cbb6903a070626c2f11b4ed53
Roberto Betancourt <dustedrob@gmail.com>
2026-07-17 17:04:18 -0700

    Request UWB_RANGING runtime permission on Android

--- composeApp/src/androidMain/kotlin/com/dustedrob/multiplatformuwb/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/dustedrob/multiplatformuwb/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.dustedrob.multiplatformuwb
 
 import App
+import UwbRangingPermission
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,6 +12,9 @@ import androidx.compose.ui.tooling.preview.Preview
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Register the UWB_RANGING permission launcher before the activity is STARTED.
+        UwbRangingPermission.register(this)
 
         setContent {
             App()
